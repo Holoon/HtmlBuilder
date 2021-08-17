3578d154f3a5301c2b430e4c6d50c9ff27aa4930
Xavier <inscription@mobius.zone>
2021-08-17 16:37:55 +0200

    Add an example

--- src/HtmlBuilder.Example/Program.cs
+++ src/HtmlBuilder.Example/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace HtmlBuilder.Example
 {
@@ -6,7 +6,37 @@ namespace HtmlBuilder.Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = Html.Div();
+
+            var h1 = Html.Heading(1);
+            h1.AddChild(Html.TextBloc("Hello World !"));
+            root.AddChild(h1);
+
+            var paragraph = Html.Paragraph();
+            var ul = Html.BulletList();
+            var li = Html.ListItem();
+            paragraph.AddChild(ul);
+            ul.AddChild(li);
+            li.AddChild(Html.TextBloc("This is a first test."));
+            root.AddChild(paragraph);
+
+            root.AddChild(Html.HorizontalRule());
+
+            root.AddChild(Html.TextBloc("This is a second test.", new InlineTag("strong", KeyValuePair.Create("class", "second"))));
+
+            var html = root.ToString();
+            /* html:
+            <div>
+                <h1>Hello World !</h1>
+                <p>
+                    <ul>
+                        <li>This is a first test.</li>
+                    </ul>
+                </p>
+                <hr />
+                <strong class="second">This is a second test.</strong>
+            </div>
+            */
         }
     }
 }
