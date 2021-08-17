c474ec4af47484b01544706f5a2c01602c602a6b
Xavier <inscription@mobius.zone>
2021-08-17 16:07:51 +0200

    Solution files

---
+++ src/HtmlBuilder.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlBuilder.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
