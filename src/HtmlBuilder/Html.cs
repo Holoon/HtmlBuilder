98ced0cb71e89d0d53b8f3b63cea4adf13df2f57
Xavier <inscription@mobius.zone>
2021-08-17 16:37:27 +0200

    Add the main project

---
+++ src/HtmlBuilder/Html.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HtmlBuilder
+{
+    public partial class Html
+    {
+        internal Html(TagBuilder builder, bool selfClosed = false)
+        {
+            SelfClosed = selfClosed;
+            TagBuilder = builder;
+            IsTag = true;
+        }
+        internal Html(HtmlContentBuilder builder)
+        {
+            ContentBuilder = builder;
+            IsTag = false;
+        }
+        internal bool IsTag { get; }
+        internal bool SelfClosed { get; }
+        internal TagBuilder TagBuilder { get; }
+        internal HtmlContentBuilder ContentBuilder { get; }
+        public IEnumerable<Html> Children { get; } = new List<Html>();
+        public void AddChild(Html child) => ((List<Html>)Children).Add(child);
+        public void SetText(string text)
+        {
+            if (IsTag && TagBuilder != null)
+                TagBuilder.InnerHtml.AppendLine(text);
+            else if (!IsTag && ContentBuilder != null)
+                ContentBuilder.AppendLine(text);
+        }
+
+        public override string ToString()
+        {
+            using (var writer = new StringWriter())
+            {
+                RenderHtml().WriteTo(writer, System.Text.Encodings.Web.HtmlEncoder.Default);
+                writer.Flush();
+                return writer.ToString();
+            }
+        }
+        public virtual HtmlContentBuilder RenderHtml()
+        {
+            var builder = new HtmlContentBuilder();
+            foreach (var htmlContent in RenderRecursively(this))
+                builder.AppendHtml(htmlContent);
+            return builder;
+        }
+        private static IEnumerable<IHtmlContent> RenderRecursively(Html html)
+        {
+            if (html == null)
+                yield break;
+
+            if (html.IsTag && html.TagBuilder != null)
+            {
+                if (html.SelfClosed && (html.Children == null || !html.Children.Any()))
+                {
+                    yield return html.TagBuilder.RenderSelfClosingTag();
+                    yield break;
+                }
+
+                yield return html.TagBuilder.RenderStartTag();
+                yield return html.TagBuilder.RenderBody();
+            }
+            else if (!html.IsTag && html.ContentBuilder != null)
+                yield return html.ContentBuilder;
+
+            foreach (var child in html.Children ?? Array.Empty<Html>())
+                foreach (var htmlContent in RenderRecursively(child))
+                    yield return htmlContent;
+
+            if (html.IsTag && html.TagBuilder != null)
+                yield return html.TagBuilder.RenderEndTag();
+        }
+    }
+}
---
+++ src/HtmlBuilder/InlineTag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HtmlBuilder
+{
+    public class InlineTag
+    {
+        public InlineTag(string tag, params KeyValuePair<string, string>[] attributes)
+        {
+            Tag = tag;
+            if (attributes != null && attributes.Any())
+            {
+                var dictionary = new Dictionary<string, string>();
+                foreach (var attribute in attributes)
+                    dictionary.Add(attribute.Key, attribute.Value);
+                Attributes = attributes;
+            }
+        }
+        public string Tag { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> Attributes { get; set; }
+    }
+}
